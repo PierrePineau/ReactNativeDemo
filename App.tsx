707b4c07475760e1ff90685f466db1d3ca3b8f99
PierrePineau <pierrepineau.pro@gmail.com>
2024-09-25 22:22:56 +0200

    Gluestack

--- App.tsx
+++ App.tsx
@@ -1,124 +1,120 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
+import "./global.css"
 import type {PropsWithChildren} from 'react';
 import {
-  Button,
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
+    Button,
+    SafeAreaView,
+    ScrollView,
+    StatusBar,
+    StyleSheet,
+    Text,
+    useColorScheme,
+    View,
 } from 'react-native';
 
 import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
+    Colors,
+    DebugInstructions,
+    Header,
+    LearnMoreLinks,
+    ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 // import Compteur from './src/screens/Compteur';
 import {
-  NavigationContainer,
-  useNavigationContainerRef,
+    NavigationContainer,
+    useNavigationContainerRef,
 } from '@react-navigation/native';
 import Compteur from './src/components/Compteur';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {enableScreens} from 'react-native-screens';
 import Home from './src/screens/Home';
 import Setting from './src/screens/Setting';
+import { GluestackUIProvider } from './src/components/ui/gluestack-ui-provider';
 
 enableScreens();
 
 const Tab = createBottomTabNavigator();
 
 type SectionProps = PropsWithChildren<{
-  title: string;
+    title: string;
 }>;
 
 function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
+    const isDarkMode = useColorScheme() === 'dark';
+    return (
+        <View style={styles.sectionContainer}>
+            <Text
+                style={[
+                    styles.sectionTitle,
+                    {
+                        color: isDarkMode ? Colors.white : Colors.black,
+                    },
+                ]}>
+                {title}
+            </Text>
+            <Text
+                style={[
+                    styles.sectionDescription,
+                    {
+                        color: isDarkMode ? Colors.light : Colors.dark,
+                    },
+                ]}>
+                {children}
+            </Text>
+        </View>
+    );
 }
 
 function HomeScreen() {
-  return (
-    // <SafeAreaView>
+    return (
+        // <SafeAreaView>
         <Home />
-    // </SafeAreaView>
-  );
+        // </SafeAreaView>
+    );
 }
 
 function SettingsScreen() {
     return (
-    //   <SafeAreaView>
+        //   <SafeAreaView>
         <Setting />
-    //   </SafeAreaView>
+        //   </SafeAreaView>
     );
-  }
-  
+}
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+    sectionContainer: {
+        marginTop: 32,
+        paddingHorizontal: 24,
+    },
+    sectionTitle: {
+        fontSize: 24,
+        fontWeight: '600',
+    },
+    sectionDescription: {
+        marginTop: 8,
+        fontSize: 18,
+        fontWeight: '400',
+    },
+    highlight: {
+        fontWeight: '700',
+    },
 });
 
 export default function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+    const isDarkMode = useColorScheme() === 'dark';
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+    const backgroundStyle = {
+        backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    };
 
-  return (
-        <NavigationContainer>
-            <Tab.Navigator screenOptions={{headerShown: false}}>
-                <Tab.Screen name="Home" component={HomeScreen} />
-                <Tab.Screen name="Settings" component={SettingsScreen} />
-            </Tab.Navigator>
-        </NavigationContainer>
-  );
+    return (
+        <GluestackUIProvider>
+            <NavigationContainer>
+                <Tab.Navigator screenOptions={{headerShown: false}}>
+                    <Tab.Screen name="Home" component={HomeScreen} />
+                    <Tab.Screen name="Settings" component={SettingsScreen} />
+                </Tab.Navigator>
+            </NavigationContainer>
+        </GluestackUIProvider>
+    );
 }
--- src/screens/Setting.tsx
+++ src/screens/Setting.tsx
@@ -1,12 +1,16 @@
-import React, {PropsWithChildren} from 'react';
+import { NavigationContainer, useNavigation, useNavigationContainerRef } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React, {Component, PropsWithChildren} from 'react';
 import {
   SafeAreaView,
   ScrollView,
   StatusBar,
   StyleSheet,
+  FlatList,
   Text,
   useColorScheme,
   View,
+  Button,
 } from 'react-native';
 
 import {
@@ -24,7 +28,7 @@ type SectionProps = PropsWithChildren<{
 function Section({children, title}: SectionProps): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
   return (
-    <View style={styles.sectionContainer}>
+    <View>
       <Text
         style={[
           styles.sectionTitle,
@@ -47,50 +51,132 @@ function Section({children, title}: SectionProps): React.JSX.Element {
   );
 }
 
-function Setting(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+const DATA = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'Profil',
+    stack: 'Profil',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    title: 'Notifications',
+    stack: 'Notification',
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    title: 'Confidentialité',
+    stack: 'Confidentialité',
+  },
+];
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+const Stack = createNativeStackNavigator();
 
-  return (
-    <View>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Section title="Step Lorem">
-          Edit oqiyuybzbruf <Text style={styles.highlight}>App.tsx</Text> to
-          change this screen and then come back to see your edits.
-        </Section>
-      </ScrollView>
-    </View>
-  );
+type ItemProps = {
+    title: string,
+    stack: string,
+};
+
+function Item({title, stack}: ItemProps) {
+    const navigationRef = useNavigation();
+    return (
+        <View style={styles.item}>
+            <Button
+                title={title}
+                onPress={() => navigationRef.navigate(stack as never)}
+            />
+        </View>
+    );
+};
+
+function SettingStack() {
+    return (
+        <View style={[styles.sectionContainer, ]}>
+            <Section title="Setting">
+                <Text style={styles.color}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit
+                </Text>
+            </Section>
+            <FlatList
+                    data={DATA}
+                    renderItem={({item}) => <Item title={item.title} stack={item.stack} />}
+                    keyExtractor={item => item.id}
+                    style={styles.list}
+                />
+        </View>
+    );
+}
+
+function ProfilStack() {
+    return (
+        <View style={[styles.sectionContainer]}>
+            <Text>Profil info Screen lorem ipsum</Text>
+        </View>
+    );
+}
+function PrivateStack() {
+    return (
+        <View style={[styles.sectionContainer]}>
+            <Text>Private stack</Text>
+        </View>
+    );
+}
+function NotificationStack() {
+    return (
+        <View style={[styles.sectionContainer]}>
+            <Text>Notification stcak</Text>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
+  color: {
+  },
   sectionContainer: {
-    marginTop: 0,
-    paddingHorizontal: 0,
-    width: '100%',
+    padding: 12,
     flex: 1,
   },
   sectionTitle: {
+    color: '#333',
     fontSize: 24,
     fontWeight: '600',
   },
   sectionDescription: {
+    color: '#333',
     marginTop: 8,
+    marginBottom: 8,
     fontSize: 18,
     fontWeight: '400',
   },
   highlight: {
     fontWeight: '700',
   },
+  list: {},
+  item: {
+    // backgroundColor: '#FFFFFF15',
+    marginVertical: 8,    
+    // marginTop: 6,
+    // marginBottom: 6,
+  },
+  title: {
+    fontSize: 18,
+  },
 });
 
-export default Setting;
+export default function Setting(): React.JSX.Element {
+    const isDarkMode = useColorScheme() === 'dark';
+  
+    const backgroundStyle = {
+      backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    };
+  
+    return (
+      <SafeAreaView style={[styles.sectionContainer, backgroundStyle]}>
+            <Stack.Navigator>
+                <Stack.Screen name="Setting" component={SettingStack} />
+                <Stack.Screen name="Profil" component={ProfilStack} />
+                <Stack.Screen name="Notification" component={NotificationStack} />
+                <Stack.Screen name="Confidentialité" component={PrivateStack} />
+            </Stack.Navigator>
+      </SafeAreaView>
+    );
+};
